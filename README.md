a23c41e69218f993578a8e4e8ebcc3fef55bf60f
Anton Pomanytskyi <antonpomanytskyi@MacBook-Pro-Anton.local>
2020-04-05 20:05:07 +0300

    add gitignore and some structure

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # RefreshKnowledge
+Here you can find some code which was created just for some educational goals.
---
+++ src/main/java/theme/pattern/observer/Observer.java
@@ -0,0 +1,7 @@
+package theme.pattern.observer;
+
+public class Observer {
+    public static void main(String[] args) {
+        System.out.println("Here will be implemented Observer pattern in nearest future))");
+    }
+}
